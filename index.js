453cb3705a280b28e6f447d443a1f1253b72ba3d
Swarup Karmokar <swarupkarmokar123@gmail.com>
2023-04-05 20:31:29 +0530

    modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const errorHandler = require('./middleware/errorHandler')
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 // DATABASE CONNECTION 
 const db = require('./config/mongoose');
